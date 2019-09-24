e969f1304552859d03d2623b6709eb7dcf72f9fb
itayhau <itayhau@gmail.com>
2019-09-24 07:59:19 +0300

    Update Program.cs

--- CSharp_bearer/Program.cs
+++ CSharp_bearer/Program.cs
@@ -26,15 +26,8 @@ namespace ConsoleApp1
             client.DefaultRequestHeaders.Accept.Add(
                 new MediaTypeWithQualityHeaderValue("application/json"));
 
-
-            string authInfo = "itay:1234";
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-
             client.DefaultRequestHeaders.Authorization =
                 new AuthenticationHeaderValue("Bearer", "Uq1e4d7-d2ckB57m8OgzGk-lIZiBjG4gvAylHlIs6EVG3UBAMCB87nB0S047tFVJlcNFyuwk1pV27c8dCwJ_oNplQCFQoy0c7pFRG4i4O4K4uyuGpegnEEZ-mQ15KiTjJ1dL_xQfiw-7YGS-X_atj53jmLBcrnLfSKbzmUFeHSKdXPYCPJzTcJ5JpET6utFUbXYlBDql9pdSfZGIEN566pcAgqNPGMN9zfEH2PViJap67pbzIy0rOoilZgIqZGEuFhLmKW19nA6gKIsuIAZioIQ655KfKtAXNkuoTyWAVVB7MgTkmtOQDzke67W7FRjStBP29koWmjjKUeO1qvSTWd6RJERFreqqXMzl0D1vVPb2tLjBbf-Hv8nkiWEHHX-s9LydSGdaMak80z2mXBLSULTpgHwvDN1wSllGn9mrxT0vuzDgenc7hSqDuHnldrSM5HSnwFxQMDdnd9ZAj-k4jCt1vOVjyfJzcHWe_D4qoXWIkaJLB0sXu0eOYwk0QAUp");
-
-            //client.DefaultRequestHeaders.Authorization =
-            //new AuthenticationHeaderValue("Basic", authInfo);
 
             // List data response.
             HttpResponseMessage response = client.GetAsync("").Result;  // Blocking call! Program will wait here until a response is received or a timeout occurs.
