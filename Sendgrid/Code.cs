a2619f03a39a429889efcdfaf6dea5beeb680c5d
itayhau <itayhau@gmail.com>
2019-10-06 09:28:47 +0300

    Update Code.cs

--- Sendgrid/Code.cs
+++ Sendgrid/Code.cs
@@ -1,17 +1,31 @@
- private static async Task Go()
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendEmail
+{
+    class Program
+    {
+
+        private static async Task Go()
         {
-            var client = new SendGridClient("......................");
+            //var apiKey = Environment.GetEnvironmentVariable("NAME_OF_THE_ENVIRONMENT_VARIABLE_FOR_YOUR_SENDGRID_KEY");
+            var client = new SendGridClient("");
             var from = new EmailAddress("test@example.com", "Example User");
             var subject = "Sending with SendGrid is Fun";
             var to = new EmailAddress("itayhau@gmail.com", "Example User");
             var plainTextContent = "and easy to do anywhere, even with C#";
-            var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
+            var htmlContent = "Click here to verify your email:<br>https://localhost:44362/Home/ConfirmEmail?id=asjhdgaksjhdgakjgdaksjgdasdj<br>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
         }
-
         static void Main(string[] args)
         {
-
             Go().Wait();
-	}
+        }
+    }
+}
